6a790edb3561eb612fad909be1e9be0853216101
s-home <seiyagutentag@gmail.com>
2020-05-13 13:49:18 +0900

    first commit

--- index.py
+++ index.py
@@ -13,9 +13,11 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(
-    'A1BexksSR3gLj9iRRAQrPcI+2gghvj/TTavfeDhUX6voG7Nh58yBGSt9EGm9J77uhc5LrL4tqnnYnZiNXfP/qa0HCnD5z/eU0kasPXY5bTCxrqAAfc2QWbv+GQxpBlyOqCimp3EdI7/DWxBN2i3X+QdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('b5aab3be7b01bb814444953a8a4e4a17')
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["A1BexksSR3gLj9iRRAQrPcI+2gghvj/TTavfeDhUX6voG7Nh58yBGSt9EGm9J77uhc5LrL4tqnnYnZiNXfP/qa0HCnD5z/eU0kasPXY5bTCxrqAAfc2QWbv+GQxpBlyOqCimp3EdI7/DWxBN2i3X+QdB04t89/1O/w1cDnyilFU="]
+YOUR_CHANNEL_SECRET = os.environ["b5aab3be7b01bb814444953a8a4e4a17"]
+
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 
 @app.route("/callback", methods=['POST'])
