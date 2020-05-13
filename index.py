b70f664fc9faeddb63bf12370b96f12ca853cb06
s-home <seiyagutentag@gmail.com>
2020-05-13 18:20:07 +0900

    commit

--- index.py
+++ index.py
@@ -5,14 +5,11 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import (ImageMessage, MessageEvent, TextMessage,
                             TextSendMessage)
-# from vision import get_text_by_ms
 from google.cloud import vision
-import base64
-import json
 import requests
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
-API_KEY = os.environ["GOOGLE_API_KEY"]
+KEY = os.environ["COMPUTER_VISION_API_KEY"]
+endpoint = 'https://eastasia.api.cognitive.microsoft.com/vision/v1.0/ocr'
 
 app = Flask(__name__)
 
@@ -55,12 +52,8 @@ def handle_image(event):
     image = BytesIO(message_content.content)
 
     try:
-        test = base64.b64encode(image)
-        print(test)
         image_text = get_text_by_ms(image)
-        print('1')
         message = TextSendMessage(text=image_text)
-        print('2')
         reply_message(event, message)
 
     except Exception as e:
@@ -74,57 +67,71 @@ def reply_message(event, messages):
     )
 
 
-def get_text_by_ms(image_url):
-    api_url = 'https://vision.googleapis.com/v1/images:annotate?key={}'.format(
-        API_KEY)
-    print('before open')
-    # imgUrl = 'https://www.python.org/static/img/python-logo.png'
-    # r = requests.get(imgUrl)
-    # img = open(r, 'rb')
-    # img_byte = img.read()
-    # img_content = base64.b64encode(img_byte)
-    # print('img')
-    # with open(image_url, "rb") as img:
-    #     print('opened')
-    #     image_content = base64.b64encode(img.read())
-    req_body = json.dumps({
-        'requests': [{
-            'image': {
-                # base64でエンコードしたものjsonにするためdecodeする
-                'content': img_content
-            },
-            'features': [{
-                'type': 'TEXT_DETECTION'
-            }]
-        }]
-    })
-    print('changed')
-    res = requests.post(api_url, data=req_body)
-    res_json = res.json()
-    labels = res_json['responses'][0]['labelAnnotations']
-    return labels
+def get_text_by_ms(image_url=None, image=None):
+    if image_url is None and image is None:
+        return '必要な情報が足りません'
 
-    # if image_url == None and image == None:
-    #     return '必要な情報が足りません'
+    params = {'visualFeatures': 'Categories,Description,Color'}
 
-    # if image_url:
-    #     image = vision.types.Image()
-    #     image.source.image_uri = image_url
-    #     response = client.document_text_detection(image=image)
-    #     texts = response.full_text_annotation.text
-    #     if response.error.message:
-    #         raise Exception(
-    #             '{}\nFor more info on error messages, check: '
-    #             'https://cloud.google.com/apis/design/errors'.format(
-    #                 response.error.message))
+    if image_url:
+        headers = {
+            'Ocp-Apim-Subscription-Key': KEY1,
+            'Content-Type': 'application/json',
+        }
+        data = {'url': image_url}
+        response = requests.post(
+            endpoint,
+            headers=headers,
+            params=params,
+            json=data
+        )
 
-    # elif image is not None:
-    #     image = vision.types.Image()
-    #     image.source.image_uri = image_url
-    #     response = client.document_text_detection(image=image)
-    #     texts = response.full_text_annotation.text
+    elif image is not None:
+        headers = {
+            'Ocp-Apim-Subscription-Key': KEY1,
+            "Content-Type": "application/octet-stream"
+        }
+        response = requests.post(
+            endpoint,
+            headers=headers,
+            params=params,
+            data=image,
+        )
 
-    # return texts
+    status = response.status_code
+    data = response.json()
+
+    if status != 200:
+
+        if data['code'] == 'InvalidImageSize':
+            text = '画像のサイズが大きすぎます'
+
+        elif data['code'] == 'InvalidImageUrl':
+            text = 'この画像URLからは取得できません'
+
+        elif data['code'] == 'InvalidImageFormat':
+            text = '対応していない画像形式です'
+
+        else:
+            text = 'エラーが発生しました'
+
+        print(status, data)
+        return text
+
+    text = ''
+    for region in data['regions']:
+        for line in region['lines']:
+            for word in line['words']:
+                text += word.get('text', '')
+                if data['language'] != 'ja':
+                    text += ' '
+        text += '\n'
+
+    if len(text) == 0:
+        text += '文字が検出できませんでした'
+
+    print('text:', text)
+    return text
 
 
 if __name__ == "__main__":
